63a15105de8997e3d1c645f48d42798859ea7dd0
kirinico <kirinico@gmail.com>
2023-04-26 19:28:27 -0300

    se implemento el delete de tasks

--- service/controller/tasksController.js
+++ service/controller/tasksController.js
@@ -1,17 +1,15 @@
-import Taks from "../models/Tasks"
+import Tasks from "../models/Tasks"
 
 
 export const postTasks = async (req, res) => {
   try {
     const product = req.body
-    const newProduct = await Taks.create(product)
+    const newProduct = await Tasks.create(product)
 
     if (!newProduct) return res.json({ msg: "no se a podido crear el producto" });
 
 
     res.json({ msg: "se a agregado el producto correctamente" });
-
-    
 
   } catch (error) {
     console.log(error);
@@ -23,9 +21,30 @@ export const getTasks = async (req, res) => {
 
   try {
 
-    const tasks = await Taks.find({})
+    const tasks = await Tasks.find({})
     res.json({ tasks });
   } catch (error) {
     res.status(500).send("hubo un error");
   }
+};
+
+export const deleteTasks = async (req, res) => {
+  try {
+    const _id = req.params.tanksId
+    const task = await Tasks.find({ _id });
+    //si el producto existe o no
+    if (!task[0]) {
+      return res.status(404).json({ msg: "no existe ese producto" });
+    }
+    const deletedTask = await Tasks.findByIdAndDelete(_id)
+    if (!deletedTask) {
+      return res.status(404).json({ msg: "no se a podido eliminar el producto" });
+    }
+
+    res.json({ msg: "se a eliminado el producto correctamente" });
+
+  } catch (error) {
+    res.status(500).send("hubo un error");
+  }
+
 };
--- service/routes/tasks.js
+++ service/routes/tasks.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { postTasks, getTasks } from '../controller/tasksController'
+import { postTasks, getTasks, deleteTasks } from '../controller/tasksController'
 const router = Router()
 
 router.post("/", postTasks)
 router.get("/", getTasks)
+router.delete("/:tanksId", deleteTasks)
+
 
 module.exports = router

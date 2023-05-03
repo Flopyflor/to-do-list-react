b002dfdd62fe12c462ef2f42a610d3806d4d4f3a
kirinico <kirinico@gmail.com>
2023-05-03 20:35:08 -0300

    se corrigio un error de categorias

--- service/config/initialSetup.js
+++ service/config/initialSetup.js
@@ -6,9 +6,9 @@ export const createCateries = async () => {
         const count = await Category.estimatedDocumentCount()
         if (count > 0) return
         const values = await Promise.all([
-            new Category({ name: "Rojo" }).save(),
-            new Category({ name: "Verde" }).save(),
-            new Category({ name: "Amarillo" }).save()
+            new Category({ name: "Red" }).save(),
+            new Category({ name: "Green" }).save(),
+            new Category({ name: "Yellow" }).save()
 
         ])
 
--- service/validators/Tasts.js
+++ service/validators/Tasts.js
@@ -2,7 +2,7 @@ import { check } from "express-validator"
 import { validateResult } from "./validateResult"
 
 export const ValidatePostProducts = [
-    check("name").exists().notEmpty(),
+    check("name").exists().notEmpty().isLength({ min: 3 }),
     check("description").exists().notEmpty(),
     
     // MM-DD-YYYY

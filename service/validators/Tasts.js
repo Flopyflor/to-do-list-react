4b644b8c0ffbdb435a2904834ab445808665cc2d
kirinico <kirinico@gmail.com>
2023-05-03 20:23:01 -0300

    se corrigio la categoria

--- service/validators/Tasts.js
+++ service/validators/Tasts.js
@@ -4,6 +4,8 @@ import { validateResult } from "./validateResult"
 export const ValidatePostProducts = [
     check("name").exists().notEmpty(),
     check("description").exists().notEmpty(),
+    
+    // MM-DD-YYYY
     check("deadLine").exists().notEmpty(),
 
     (req, res, next) => {
